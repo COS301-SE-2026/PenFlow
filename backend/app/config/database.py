be423a018cf187ca5ecdd216f499ff6ee8985b02
AyrTal <u24856462@tuks.co.za>
2026-05-17 17:31:38 +0200

    Configured report gen for backend and updated infrastructure

--- backend/app/config/database.py
+++ backend/app/config/database.py
@@ -1,7 +1,12 @@
 import os
 
+from pathlib import Path
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
+ROOT_DIR = Path(__file__).resolve().parents[3]
+load_dotenv(ROOT_DIR / ".env")
 
 DATABASE_URL = os.getenv("DATABASE_URL");
 
