a9dde78dafdb2d6061b25989b9eae3eddcf2684d
23542773 <u23542773@tuks.co.za>
2026-05-18 16:56:23 +0200

    Setting up pytest for my unit testing

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,5 +1,17 @@
 import sys
 from pathlib import Path
+import pytest
+from fastapi.testclient import TestClient
+from app.main import app
+
+@pytest.fixture(scope="module")
+def test_client():
+    """
+    TestsClient instance, to simulate API requests
+    """
+
+    with TestClient(app) as client:
+        yield client
 
 ROOT_DIR = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(ROOT_DIR))
