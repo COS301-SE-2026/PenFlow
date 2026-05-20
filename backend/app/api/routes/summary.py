83f1de3cefd1a64ed1c0c342e9baecb207cb8f94
23542773 <u23542773@tuks.co.za>
2026-05-20 04:17:50 +0200

    Added router that exposes the endpoint

---
+++ backend/app/api/routes/summary.py
@@ -0,0 +1,27 @@
+from uuid import UUID
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.utils.db import get_db
+from app.schemas.summary import ScanSummary
+from app.repositories import summary_repo
+
+router = APIRouter(prefix="/scans", tags=["Executive Summary"])
+
+@router.get("/{scan_id}/summary", response_model=ScanSummary, status_code=status.HTTP_200_OK)
+async def get_scan_summary(
+    scan_id: UUID,
+    db: AsyncSession = Depends(get_db)
+):
+    """
+    Generates  complete Executive summary for a specific scan.
+    """
+    scan_data = await summary_repo.get_scan_summary(db, scan_id)
+
+    if not scan_data:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Scan with ID {scan_id} not found."
+
+        )
+        return scan_data
