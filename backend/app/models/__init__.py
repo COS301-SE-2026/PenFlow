63d8772178fbce2c4bde64b1ae648bdcd75e2be7
Franky <u24673898@tuks.co.za>
2026-05-22 01:07:51 +0200

    fix indenatation error  and wrong class import

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,8 +1,8 @@
 # type: ignore
-from app.models.asset import Asset
+from app.models.asset import Asset as Asset
 from app.models.base import Base, ScanStatus, Severity
-from app.models.finding import Finding
-from app.models.scan import Scan
-from app.models.user import User
+from app.models.finding import Finding as Finding
+from app.models.scan import Scan as Scan
+from app.models.user import User as User
 
-__all__ = ["Base", "ScanStatus", "Severity", "Asset", "Finding", "Scan", "User"]
+__all__ = ["Base", "ScanStatus", "Severity", "Asset", "Finding", "Scan" , "User"]
