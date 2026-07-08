0221c9e5d0b8a34dbabd4f81c1556a49c8e7e80a
jeandre <u23542773@tuks.co.>
2026-07-08 22:24:28 +0200

    Unit test: Test get scan by id success

--- backend/tests/unit/test_scan_repo.py
+++ backend/tests/unit/test_scan_repo.py
@@ -17,3 +17,13 @@ async def test_create_scan_success(db_session):
     assert new_scan.email == email
     assert new_scan.status == ScanStatus.QUEUED
     assert new_scan.progress == 0
+
+@pytest.mark.asyncio
+async def test_get_scan_by_id_success(db_session):
+
+    domain = "pen-flow.com"
+    new_scan = await ScanRepository.create_scan(db_session, domain)
+
+    assert retrieed_scan is not None
+    assert retrieved_scan.id == new_scan.id
+    assert retrieved_scan.domain == domain
