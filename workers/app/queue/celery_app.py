1d8bf8bb1c098b3734607c64547b97efb9204e77
Locutus-0201 <u24564738@tuks.co.za>
2026-07-10 19:35:50 +0200

    feature: registering domain verification task with celery

--- workers/app/queue/celery_app.py
+++ workers/app/queue/celery_app.py
@@ -15,6 +15,7 @@ celery_app = Celery(
         "app.tasks.shodan_tasks",
         "app.tasks.hunter_tasks",
         "app.tasks.hibp_tasks",
+        "app.tasks.domain_verification_task",
         "app.tasks.full_scan_tasks",
     ],
 )
