83ca30f373c37204285f6ef145eab9c6fc96b6c3
Locutus-0201 <u24564738@tuks.co.za>
2026-07-17 19:13:32 +0200

    Chore: Added technologies discovered from doing tests scans

--- workers/app/services/signatures/servers.py
+++ workers/app/services/signatures/servers.py
@@ -37,5 +37,21 @@ SERVER_SIGNATURES = {
                 "regex": r"apache/([\d\.]+)"
             }
         ]
-    }
+    },
+    "Apache Tomcat": {
+        "category": "web_server",
+        "vendor": "apache",
+        "product": "tomcat",
+        "headers": [("server", "coyote", 90)],
+        "version_extractors": [{"type": "header", "target": "server", "regex": r"coyote/([\d\.]+)"}]
+    },
+    "Cloudflare": {
+        "category": "web_server",
+        "vendor": "cloudflare",
+        "product": "cloudflare",
+        "headers": [
+            ("server", "cloudflare", 90)
+        ],
+        "version_extractors": []
+    },
 }
