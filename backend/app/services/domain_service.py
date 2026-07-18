7c70f61d7615addb3ed39651b0de59a8730b6c42
AyrTal <u24856462@tuks.co.za>
2026-07-18 13:27:04 +0200

    Added domain addition in domain service

--- backend/app/services/domain_service.py
+++ backend/app/services/domain_service.py
@@ -1,4 +1,13 @@
+from uuid import UUID
+
 from fastapi import HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models.verified_domain import VerifiedDomain
+
+from app.repositories.domain_repository import DomainRepository
+
+from app.services.verification_service import VerificationService
 
 class DomainService:
 
@@ -23,3 +32,28 @@ class DomainService:
         
         return stripped
     
+
+    @staticmethod
+    async def add_domain(db: AsyncSession, domain: str, user_id: UUID) -> VerifiedDomain:
+        stripped_domain = DomainService.strip_domain(domain)
+
+        existing_domain = await DomainRepository.get_by_domain(
+            db,
+            domain = stripped_domain,
+            user_id = user_id,
+        )
+
+        if existing_domain is not None:
+            raise HTTPException(
+                status_code = status.HTTP_409_CONFLICT,
+                detail = "This domain has already been added",
+            )
+        
+        token = VerificationService.generate_txt_token()
+
+        return await DomainRepository.create_rec(
+            db,
+            domain = stripped_domain,
+            verification_token = token,
+            user_id = user_id,
+        )
