71873a6e3f377050cc15739584cf33fc51e8d99f
AyrTal <u24856462@tuks.co.za>
2026-07-18 20:26:19 +0200

    Fixed extra comma

--- docker/postgres/init/01-init-schema.sql
+++ docker/postgres/init/01-init-schema.sql
@@ -68,7 +68,7 @@ CREATE TYPE domain_verification_code AS ENUM (
 
 CREATE TABLE organisations (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name VARCHAR(255) NOT NULL,,
+    name VARCHAR(255) NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
