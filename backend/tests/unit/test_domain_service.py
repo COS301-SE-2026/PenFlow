2ba24fc1ab6f05b82dba54d57edd339d6aed9203
AyrTal <u24856462@tuks.co.za>
2026-07-21 01:41:55 +0200

    Added test for invalid sanitizing/stripping domain of chars

--- backend/tests/unit/test_domain_service.py
+++ backend/tests/unit/test_domain_service.py
@@ -1,4 +1,6 @@
 import pytest
+from fastapi import HTTPException
+
 from app.services.domain_service import DomainService
 
 @pytest.mark.parametrize(
@@ -17,3 +19,21 @@ def test_strip_domain(domain, expected):
     result = DomainService.strip_domain(domain)
 
     assert result == expected
+
+
+@pytest.mark.parametrize(
+    "domain",
+    [
+        "",
+        " ",
+        "https://",
+        "http://",
+    ],
+)
+
+def test_strip_domain_invalid(domain):
+    with pytest.raises(HTTPException) as excep:
+        DomainService.strip_domain(domain)
+
+    assert excep.value.status_code == 422
+    assert excep.value.detail == "A valid domain is needed"
