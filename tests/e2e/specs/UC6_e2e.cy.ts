08ac8bd27f0b6727e6376c2f7c5d8af091a17117
Franky <u24673898@tuks.co.za>
2026-07-22 14:58:30 +0200

    E2E TEST uc6 downloading a pdf report

--- tests/e2e/specs/UC6_e2e.cy.ts
+++ tests/e2e/specs/UC6_e2e.cy.ts
@@ -1,1 +1,31 @@
 //Download PDF Report
+
+describe("User log in and Download pdf report",()=>{
+    beforeEach(()=>{
+        cy.login("1234","1234")
+        cy.getCookie("logged_in").should("have.property","value","1")
+    })
+    it("Download Pdf report",()=>{
+        cy.contains("a","HISTORY").click();   
+        cy.url().should("include","/history");
+        cy.get("table tbody tr").first().click();
+        cy.contains("button","VIEW REPORT").should("be.visible").click();
+        cy.url().should("include","/report/")
+        
+        //set up the intercept capture http rquest match with **/scans/pdf
+        cy.intercept("GET","**/scans/*/pdf").as("pdfDownload")   
+
+        //checks if it have the downlaod full report options
+        cy.contains("a","DOWNLOAD FULL REPORT")
+        .should("have.attr","href")
+        .and("include","/pdf")
+        //click the link
+        cy.contains("a","DOWNLOAD FULL REPORT").click();
+
+        //verify the intercetpt request was succesfull 
+        cy.wait("@pdfDownload").its("response.statusCode")
+        .should("eq",200); //http status success
+        cy.get("@pdfDownload").its("response.headers.content-type")
+        .should("include","application/pdf");
+});
+});
