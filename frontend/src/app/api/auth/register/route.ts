5bcb984b68ea57bca5e46f499552e6e57e4ae520
Franky <u24673898@tuks.co.za>
2026-08-06 23:57:34 +0200

    Fixed the register logic

--- frontend/src/app/api/auth/register/route.ts
+++ frontend/src/app/api/auth/register/route.ts
@@ -1,27 +1,40 @@
-/*import { NextRequest, NextResponse } from "next/server";
+import {  NextResponse } from "next/server";
 
-import { createAuthResponse } from "@/lib/authSession";
-import { loginWithPassword, registerUser } from "@/lib/keycloakService";
+import {AUTHORIZATION_URL} from  "@/lib/keycloakService"
+import { generateCodeChallenge,generateCodeVerifier,generateState } 
+from "@/lib/pkce";
 
-export async function POST(req: NextRequest) {
-  const body = await req.json() as Record<string, string>;
-  const { username, email, password, firstName, lastName } = body;
+const CLIENT_ID = process.env.KEYCLOAK_CLIENT_ID ?? "penflow-web"
+const APP_URL = process.env.APP_URL ?? "http://localhost:3000";
 
-  if (!username || !email || !password || !firstName || !lastName) {
-    return NextResponse.json(
-      { error: "All fields are required" },
-      { status: 400 }
-    );
-  }
+export async function  GET() {
+  //PKCE(proof key for key exchange) Security Generation
+  const verifier = generateCodeVerifier(); //random secret string
+  const challenge = generateCodeChallenge(verifier); //hashed verision of verifier
+  const state = generateState() //csrf protection token
 
-  try {
-    await registerUser(username, email, password, firstName, lastName);
-    const tokens = await loginWithPassword(username, password);
-    return createAuthResponse(tokens);
-  } catch (err) {
-    const message = err instanceof Error ? err.message : "Registration failed";
-    const status = message.includes("already exists") ? 409 : 400;
-    return NextResponse.json({ error: message }, { status });
-  }
+
+// Keycloak Registration URL 
+const redirectUri = `${APP_URL}/api/auth/callback`;
+const registerUrl = new URL(AUTHORIZATION_URL.replace("/auth", "/registrations"));
+
+// OAuth Parameters 
+registerUrl.searchParams.set("client_id", CLIENT_ID);
+registerUrl.searchParams.set("response_type", "code"); 
+registerUrl.searchParams.set("scope", "openid profile email");
+registerUrl.searchParams.set("redirect_uri", redirectUri); 
+registerUrl.searchParams.set("state", state); 
+registerUrl.searchParams.set("code_challenge", challenge); 
+registerUrl.searchParams.set("code_challenge_method", "S256")
+
+//Store Security Tokens as Cookies
+const resp=NextResponse.redirect(registerUrl);
+const secure=process.env.NODE_ENV ==="production";
+
+
+resp.cookies.set("oauth_state", state, { httpOnly: true, secure, sameSite: "lax", path: "/", maxAge: 900 }); //900 secs
+resp.cookies.set("pkce_verifier", verifier, { httpOnly: true, secure, sameSite: "lax", path: "/", maxAge: 900 });
+
+
+  return resp;
 }
-*/
