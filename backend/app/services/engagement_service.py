7e41e1841461b9ecd479277e65cf9e9c20acfee7
jeandre <u23542773@tuks.co.>
2026-08-10 01:03:57 +0200

    feat- Added fetching all the engagements and engagements activity services

--- backend/app/services/engagement_service.py
+++ backend/app/services/engagement_service.py
@@ -1,11 +1,20 @@
-from app.repositories.engagement_repo import EngagementRepository
+from typing import List
+from app.schemas.engagement import EngagementRead, ActivityEventRead
+
 
 class EngagementService:
-    def __init__(self, repo: EngagementRepository):
+    def __init__(self, repo):
         self.repo = repo
 
-    async def get_dashboard_summary(self):
-        return await self.repo.get_aggregated_metrics()
+    async def get_all_engagements(self) -> List[EngagementRead]:
+        """
+        Fetch all engagements.
+        """
+        return await self.repo.get_all()
+        pass
 
-    async def get_historical_data(self, time_range: str):
-        return await self.repo.get_time_series(time_range = time_rnage)
+    async def get_engagement_activity(self, engagement_id: str) -> List[ActivityEventRead]:
+        """
+        Fetch the chronological event log.
+        """
+        pass
