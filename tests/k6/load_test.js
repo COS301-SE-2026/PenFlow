4b511d603aed2404475cf9af9edbf89b80ec0df5
Franky <u24673898@tuks.co.za>
2026-08-16 22:20:22 +0200

    NFR test - QR3 scalability test

--- tests/k6/load_test.js
+++ tests/k6/load_test.js
@@ -7,14 +7,25 @@ export const options = {
   scenarios: {
     //QR1 :P95 API response time under 2s under normal load
     perfromance :{
-      executor: 'constant-vus',
+      executor: 'constant-vus', //constant vus 
       vus:10, //10 virtual user
       duration: '30s',
       exec: 'checkHealth',
     },
+    // QR-03: system stays stable at 100 concurrent user
+    scalability: {
+    executor: 'ramping-vus',//ramping vus
+    startVUs: 0,
+    stages: [
+      { duration: '30s', target: 100 },
+      { duration: '1m', target: 100 },
+      { duration: '30s', target: 0 },
+    ],
+    }
   },
    thresholds: {
-  http_req_duration: ['p(95)<2000'],//qr 1 target
+  http_req_duration: ['p(95)<2000'],//qr 1 target 95 % chance less than 2ms
+   http_req_failed: ['rate<0.01'], //qr2 target 
   },
 };
 
