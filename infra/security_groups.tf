1ab0ce4cb9760d3ea12fd92fdeca2a67f378a7e0
jeandre <u23542773@tuks.co.>
2026-09-18 17:34:17 +0200

    Add: allows parent worker to connect to the database

--- infra/security_groups.tf
+++ infra/security_groups.tf
@@ -161,6 +161,14 @@ resource "aws_security_group" "rds" {
     security_groups = [aws_security_group.scheduler.id]
   }
 
+  ingress {
+    description     = "PostgreSQL from isolated scan workers" 
+    from_port       = 5432 
+    to_port         = 5432 
+    protocol        = "tcp" 
+    security_groups = [aws_security_group.worker.id]
+  }
+
   egress {
     description = "Allow outbound traffic"
     from_port   = 0
